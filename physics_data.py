651bc98bf481f3f24b6f9fc25ce923052c126781
Srivatsa Kundurthy <srivatsa.kundurthy@gmail.com>
2022-07-13 11:42:03 -0400

    physics expanded knowledge

--- physics_data.py
+++ physics_data.py
@@ -9,16 +9,16 @@ Do you believe the demonstration expanded your understanding of the material pre
 Would you benefit from greater integration of Python into this course? 18 yes, 3 no
 
 '''
-data = {'Yes':20,'No':1}
+data = {'Yes':21,'No':0}
 x = list(data.keys())
 
 y = list(data.values())
 fig = plt.figure(figsize = (10,10))
 plt.bar(x, y, width=.2)
-plt.xlabel('Were students able to follow along with the Python demonstration?')
+plt.xlabel('Do students believe the demonstration expanded understanding of the material presented?')
 plt.ylabel('Number of Students')
-plt.title('Number of Students Able to Follow Along')
-plt.savefig('physics_Follow_along.png')
+plt.title('Number of Students In/Not In Agreement')
+plt.savefig('physics_expanded_knowledge.png')
 
 #plt.show()
 
