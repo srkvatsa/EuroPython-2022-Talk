c273b2864f2c65bcb0fa202a68da0173d0a60430
Srivatsa Kundurthy <srivatsa.kundurthy@gmail.com>
2022-07-13 13:26:52 -0400

    calc greater integration

--- calc_data.py
+++ calc_data.py
@@ -10,16 +10,16 @@ Would you benefit from greater integration of Python into this course? 16 yes, 8
 
 
 '''
-data = {'Yes':19,'No':5}
+data = {'Yes':16,'No':8}
 x = list(data.keys())
 
 y = list(data.values())
 fig = plt.figure(figsize = (10,10))
 plt.bar(x, y, width=.2)
-plt.xlabel('Do students believe the demonstration expanded their understanding of the material presented?')
+plt.xlabel('Would students benefit from greater integration of Python into this course?')
 plt.ylabel('Number of Students')
 plt.title('Number of Students In/Not In Agreement with Survey Question')
-plt.savefig('calc_expanded_knowledge.png')
+plt.savefig('calc_greater_integration_ofpy.png')
 
 #plt.show()
 
