b8b56bdf54b88882cc1a5e69ac76ce49a357bd8d
Srivatsa Kundurthy <srivatsa.kundurthy@gmail.com>
2022-07-13 13:18:04 -0400

    calc prior xp

---
+++ calc_data.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+'''
+Do you have prior programming experience? 14 yes, 10 no
+Have you learned Python before? 5 yes, 19 no
+Were you able to follow along with the Python demonstration? 19 yes, 5 no
+Do you believe the demonstration expanded your understanding of the material presented? 19 yes, 5 no
+
+Would you benefit from greater integration of Python into this course? 15 yes, 9 no
+
+
+'''
+data = {'Yes':18,'No':3}
+x = list(data.keys())
+
+y = list(data.values())
+fig = plt.figure(figsize = (10,10))
+plt.bar(x, y, width=.2)
+plt.xlabel('Do students have prior programming experience?')
+plt.ylabel('Number of Students')
+plt.title('Number of Students In/Not In Agreement with Survey Question')
+plt.savefig('calc_prior_xp.png')
+
+#plt.show()
+
+
